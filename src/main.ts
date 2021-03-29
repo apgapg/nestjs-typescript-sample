6e3606ce4c427fb72acc0e3f21dc1b59f780b22f
Ayush P Gupta <ayushpguptaapg@gmail.com>
2021-03-29 16:22:51 +0530

    Add swagger for api docs
    Signed-off-by: Ayush P Gupta <ayushpguptaapg@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Setup swagger
+  const config = new DocumentBuilder()
+    .setTitle('GT App')
+    .setDescription('Official GT App APIs docs')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
